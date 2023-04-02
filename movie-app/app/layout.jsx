fca6a64bf64a9ae8d309a969ccc726be300bb7b3
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-04-02 17:33:36 +0200

    Tailwind

--- movie-app/app/layout.jsx
+++ movie-app/app/layout.jsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${montserrat.className}`}>
         <nav>
           <h1>logo</h1>
           <ul>
--- movie-app/app/page.jsx
+++ movie-app/app/page.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Home</h1>
+      <h1 className="text-lg">Home</h1>
     </main>
   );
 }
